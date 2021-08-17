533c4d8dff0fc500f8f53247442940eab3ef7e3a
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 01:41:53 +0600

    Create StringLength.c

---
+++ StringLength.c
@@ -0,0 +1,12 @@
+//length of a string
+
+#include <stdio.h>
+int main() {
+ char str[100];
+ printf("Enter a string:\n");
+ gets(str);
+ int len;
+ for (len = 0; str[len] != '\0'; len++);
+ printf("Length of the string: %d", len);
+ return 0;
+}
