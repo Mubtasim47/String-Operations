b37f6d8e1620c158dff4024e32992e43106cde17
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 02:11:02 +0600

    Create StringTokenization.c

---
+++ StringTokenization.c
@@ -0,0 +1,20 @@
+//tokenization of a string
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+ char str[] = "We are Studying Compiler Design", *token;
+ token = strtok(str, " ");
+ printf("%s\n", token);
+ token = strtok(NULL, " ");
+ printf("%s\n", token);
+ token = strtok(NULL, " ");
+ printf("%s\n", token);
+ token = strtok(NULL, " ");
+ printf("%s\n", token);
+ token = strtok(NULL, " ");
+ printf("%s\n", token);
+ return 0;
+
+}
