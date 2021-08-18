f5659d25d6042713765a331159353d4f5b5d3021
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 12:00:09 +0600

    Update StringTokenization.c

--- StringTokenization.c
+++ StringTokenization.c
@@ -2,19 +2,22 @@
 
 #include <stdio.h>
 #include <string.h>
+
 int main() {
 
- char str[] = "We are Studying Compiler Design", *token;
- token = strtok(str, " ");
- printf("%s\n", token);
- token = strtok(NULL, " ");
- printf("%s\n", token);
- token = strtok(NULL, " ");
- printf("%s\n", token);
- token = strtok(NULL, " ");
- printf("%s\n", token);
- token = strtok(NULL, " ");
- printf("%s\n", token);
- return 0;
+    char str[] = "We are Studying Compiler Design", *token;
+
+    token = strtok(str, " ");
+    printf("%s\n", token);
+    token = strtok(NULL, " ");
+    printf("%s\n", token);
+    token = strtok(NULL, " ");
+    printf("%s\n", token);
+    token = strtok(NULL, " ");
+    printf("%s\n", token);
+    token = strtok(NULL, " ");
+    printf("%s\n", token);
+
+    return 0;
 
 }
