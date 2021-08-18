1b228b24dce3b122bf6c153491979535f8094453
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 12:02:32 +0600

    Update StringTokenizationUsingLoops.c

--- StringTokenizationUsingLoops.c
+++ StringTokenizationUsingLoops.c
@@ -2,13 +2,17 @@
 
 #include <stdio.h>
 #include <string.h>
+
 int main() {
- char str[] = "There are different types of programming languages like: C, Java, Python", *token;
- token = strtok(str, ", :");
- while(token != NULL) {
- printf("%s\n", token);
- token = strtok(NULL, ", :");
- }
- return 0;
-  
+
+    char str[] = "There are different types of programming languages like: C, Java, Python", *token;
+
+    token = strtok(str, ", :");
+    while(token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, ", :");
+    }
+
+    return 0;
+
 }
