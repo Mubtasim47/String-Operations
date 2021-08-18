88b3438b7476781fc046a0883ba5cd4878df3a7f
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 11:52:50 +0600

    Update StringLength.c

--- StringLength.c
+++ StringLength.c
@@ -1,12 +1,16 @@
 //length of a string
 
 #include <stdio.h>
+
 int main() {
- char str[100];
- printf("Enter a string:\n");
- gets(str);
- int len;
- for (len = 0; str[len] != '\0'; len++);
- printf("Length of the string: %d", len);
- return 0;
+    
+    char str[100];
+    
+    printf("Enter a string:\n");
+    gets(str);
+    int len;
+    for (len = 0; str[len] != '\0'; len++);
+    printf("Length of the string: %d", len);
+    
+    return 0;
 }
