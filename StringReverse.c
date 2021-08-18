f7887c1153f412bd953eb1f6c293f33af49c35f4
Mubtasim47 <88978153+Mubtasim47@users.noreply.github.com>
2021-08-18 11:58:06 +0600

    Update StringReverse.c

--- StringReverse.c
+++ StringReverse.c
@@ -1,17 +1,22 @@
 //Reverse a given string
 
 #include <stdio.h>
+
 int main() {
- char str[50], revStr[50];
- printf("Enter a string:\n");
- gets(str);
- int len;
- for (len = 0; str[len] != '\0'; len++);
- int j = len - 1;
- for (int i = 0; i < len; i++)
- revStr[i] = str[j--];
- revStr[len] = '\0';
- printf("String after reverse: ");
- puts(revStr);
- return 0;
+
+    char str[50], revStr[50];
+
+    printf("Enter a string:\n");
+    gets(str);
+    int len;
+    for (len = 0; str[len] != '\0'; len++);
+    int j = len - 1;
+    for (int i = 0; i < len; i++)
+        revStr[i] = str[j--];
+    revStr[len] = '\0';
+    printf("String after reverse: ");
+    puts(revStr);
+
+    return 0;
+    
 }
